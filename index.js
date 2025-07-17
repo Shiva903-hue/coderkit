7fcd1314d208c9e7ecc243f7b8ed6de0177875a5
shivam waghmare <waghmareshivam184@gmail.com>
2025-07-17 08:06:16 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+ export function add(a, b) {
+  return a + b;
+}
+
+ export function multiply(a, b) {
+  return a * b;
+}
+ export function sub(a, b) {
+  return a - b;
+}
+
+
+
+// ✅ Add default export (optional)
+// export default {
+//   add,
+//   multiply,
+//   sub
+// };
