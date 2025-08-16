36fc1af98b78ed60c8b2e1808fba121b6ce301c5
shivam waghmare <waghmareshivam184@gmail.com>
2025-08-16 20:01:01 +0530

    string modules added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import * as math from './Src/Math.js';
 import * as arr from './Src/Array.js';
 import * as obj from './Src/Object.js';
+import * as str from './Src/String.js';
 
 // Re-export individual functions
 export * from './Src/Math.js';
 export * from './Src/Array.js';
 export * from './Src/Object.js';
+export * from './Src/String.js';
 
 // Also export as namespaces
-export { math, arr , obj};
+export { math, arr , str, obj};
